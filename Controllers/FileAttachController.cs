bf049185dbd4ff3260ced67788c1c1b6d4538ef0
SeyedAhmad <saadatahmad267@gmail.com>
2021-12-21 11:46:57 +0330

    method Upload File to Postgresql

--- Controllers/FileAttachController.cs
+++ Controllers/FileAttachController.cs
@@ -12,37 +12,39 @@ namespace Files.Controllers
     public class FileAttachController : ControllerBase
     {
         private readonly ApplicationDbContext _dbContext;
-        public FileAttachController( ApplicationDbContext dbContext)
+        public FileAttachController(ApplicationDbContext dbContext)
         {
             _dbContext = dbContext;
         }
 
-       public ActionResult Index (IFormFile files)
-       {
-           if(files != null)
-           {
-               if(files.Length >0){
-                   var filename = Path.GetFileName(files.FileName);
-                   var fileExtention = Path.GetExtension(filename);
-                   var newFileName = string.Concat(Convert.ToString(Guid.NewGuid()),fileExtention);
-                   var objFiles = new Files()
-                   {
-                        DocumentId =0,
-                        Name= newFileName,
-                        FileType=fileExtention,
-                        CreatedOn =  DateTime.Now
-                   };
-                   using(var target = new MemoryStream())
-                   {
-                       files.CopyTo(target);
-                       objFiles.DataFiles = target.ToArray();
-                   }
-                   _dbContext.files.Add(objFiles);
-                   _dbContext.SaveChanges();
-                   
-
-               }
-           }
-       }
+        [HttpPost]
+        public IActionResult UploadFile(IFormFile files)
+        {
+            if (files != null)
+            {
+                if (files.Length > 0)
+                {
+                    var filename = Path.GetFileName(files.FileName);
+                    var fileExtention = Path.GetExtension(filename);
+                    var newFileName = string.Concat(Convert.ToString(Guid.NewGuid()), fileExtention);
+                    var objFiles = new Files()
+                    {
+                        DocumentId = 0,
+                        Name = newFileName,
+                        FileType = fileExtention,
+                        CreatedOn = DateTime.UtcNow
+                    };
+                    using (var target = new MemoryStream())
+                    {
+                        files.CopyTo(target);
+                        objFiles.DataFiles = target.ToArray();
+                    }
+                    _dbContext.files.Add(objFiles);
+                    _dbContext.SaveChanges();
+                }
+            }
+            // return Ok("file inserted");
+            return Ok();
+        }
     }
 }
---
+++ Dto/FilesDto.cs
@@ -0,0 +1,11 @@
+namespace BinaryFile.Dto
+{
+    public class FilesDto
+    {
+        public int DocumentId { get; set; }
+        public string Name { get; set; }
+        public string FileType { get; set; }
+        public DateTime? CreatedOn { get; set; }
+
+    }
+}
